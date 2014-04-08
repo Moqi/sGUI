47cddbe2651bc2f20e84773c16fe2124eff51eaf
Sharbel <sharbelfs@gmail.com>
2014-04-08 18:42:36 -0300

    image button example

--- sGui/Assets/Scripts/sGuiImage.cs
+++ sGui/Assets/Scripts/sGuiImage.cs
@@ -7,72 +7,42 @@ using System.Collections;
 public class sGuiImage : MonoBehaviour
 {
 
-	public int _depth = 1;
-	public Rect _position;
 	public Texture _image;
 
 	public ScaleMode _scaleMode;
 
-	public GUIStyle _style = new GUIStyle();
-
-	public bool isChild = false;
 
 	void Start() {
-
 		updateStyles();
-
 	}
-
-	public void updateStyles() {
-		if (_style == null) {
-			_style = new GUIStyle();
-		}
-
-	}
-
-
 	void OnValidate() {
 		updateStyles();
 	}
+	void Awake() {
+		updateStyles();
+	}
+	void OnEnable() {
+		updateStyles();
+	}
 
-	public void childGUI(bool isModal) {
+	public void updateStyles() {
 
-		if (!this.gameObject.activeSelf) {
-			return;
+		if (this.GetComponent<sGuiBase>() != null) {
+			this.GetComponent<sGuiBase>().onGuiFunc = drawGui;
+			this.GetComponent<sGuiBase>().onChildGuiFunc = drawChildGui;
 		}
+	}
 
-		isChild = true;
-		GUI.depth = _depth;
-
+	public void drawGui(Rect position, GUIStyle style) {
 		if (_image != null) {
-			GUI.DrawTexture(_position, _image, _scaleMode, true);
-		}
-		//GUILayout.Label (_content, _style, GUILayout.Width (_position.width), GUILayout.Height (_position.height));
-
-	}
-
-	public void childGUI() {
-
-		if (!this.gameObject.activeSelf) {
-			return;
-		}
-
-		isChild = true;
-		//GUI.depth = _depth;
-		//GUILayout.Label(_content, _style, GUILayout.Width(_position.width), GUILayout.Height(_position.height));
-	}
-
-	void OnGUI() {
-
-		if (!this.gameObject.activeSelf) {
-			return;
-		}
-
-		if (!isChild) {
-			GUI.depth = _depth;
-			if (_image != null) {
-				GUI.DrawTexture(_position, _image, _scaleMode, true);
-			}
+			GUI.DrawTexture(position, _image, _scaleMode, true);
 		}
 	}
+
+	public void drawChildGui(Rect position, GUIStyle style) {
+		if (_image != null) {
+			GUI.DrawTexture(position, _image, _scaleMode, true);
+		}
+	}
+
 }
---
+++ sGui/Assets/buttonActionExample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class buttonActionExample : MonoBehaviour {
+
+
+	private int clicks;
+
+	void Start () {
+
+		this.GetComponent<sGuiButton>().onClickButton = clickButton;
+		clicks = 0;
+	}
+
+	public void clickButton(GameObject curr) {
+		Debug.Log("button clicked");
+		clicks++;
+		this.GetComponent<sGuiButton>().Content.text = "clicked " + clicks;
+	}
+	
+	void Update () {
+	
+	}
+}
